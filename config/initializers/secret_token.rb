4063c80719fc245c50be11c5d9d48569937179c5
Chris Sund <chris@silhouettesolutions.net>
2011-12-05 19:27:40 -0700

    add source files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Valier::Application.config.secret_token = '1b353ee30c4a667d912d142d2c2295f12b70b5a6acd11721cfda3e637990bd66d1a938d6058aee3101b848956d46a3d5e3edfdb976544ee9287f72d1761c334a'
---
+++ public/javascripts/cufon-replace.js
@@ -0,0 +1,2 @@
+Cufon.replace('#menu li a, #menu_active, h2, .link, .font1', { fontFamily: 'RomanSerif', hover:true });
+
